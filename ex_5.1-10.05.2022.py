d43c91aa78277a22e34044c9ee4aac8c903e6620
vadim romanyuk <v.romanyuk@borosp.by>
2022-05-24 15:29:10 +0300

    задание на + - через функцию

---
+++ ex_5.1-10.05.2022.py
@@ -0,0 +1,29 @@
+def pos_neg(input_number: str):
+# заменяем , на .
+    number = input_number.replace(',', '.')
+# проверяем на кол-во точек и дефисов
+    if number.count("." and "-") <= 1:
+        if "-" or "" in number[0]:
+            # заменяем -, . на пустоту и проверяем число ли перед нами
+                a = number.replace('-', '')
+                if (a.replace('.', '').isdigit()) is True:
+                    if float(number) > 0:
+                        return 'you entered a positive number'
+                    elif float(number) < 0:
+                        return 'you entered a negative number'
+                    else:
+                        return 'you entered zero'
+                else:
+                    return "you didn't enter a number4"
+        else:
+            return "you didn't enter a number2"
+    else:
+        return "you didn't enter a number1"
+
+
+input_number = input('inset a number: ')
+print(pos_neg(input_number))
+
+
+
+
