d3a528b34a63f6dd1a3b0ac3f75018b3c3aa2991
vadim romanyuk <v.romanyuk@borosp.by>
2022-05-25 19:27:28 +0300

    добавил 2второе задание. class Car Track, со своими атрибутами а так же некоторыми новыми методами. с задержкой вывода текста

--- lesson_8/ex_7.1-24.05.2022.py
+++ lesson_8/ex_7.1-24.05.2022.py
@@ -1,5 +1,7 @@
-class Auto:
+import time
 
+
+class Auto:
 # задаем атрибуты
     def __init__(self, brand, age, mark, color='black', weight=1700):
         self.brand = brand
@@ -20,13 +22,46 @@ class Auto:
         print('stop')
 
 
+class Truck(Auto):
+    def __init__(self, brand, age, mark, max_load, color='black', weight=1700, ):
+        self.max_load = max_load
+        Auto.__init__(self, brand, age, mark, color='black', weight=1700)
+
+    def move(self):
+        print('attention move')
+
+    def load(self):
+        time.sleep(1)
+        print('load')
+        time.sleep(1)
+
+
+class Car(Auto):
+    def __init__(self, brand, age, mark, max_speed, color='black', weight=1700, ):
+        self.max_speed = max_speed
+        Auto.__init__(self, brand, age, mark, color='black', weight=1700)
+
+    def move(self):
+        print('attention move')
+        print('max speed is ' + str(self.max_speed))
+
+
 skoda_car = Auto(brand='Skoda', age=3, color='red', mark='rapid', weight=1700)
 opel_car = Auto('Opel', 99, 'black', 'Vectra', 2500)
+track_daf = Truck('DAF', 25, 'sn1236', 3700)
+track_volvo = Truck('Volvo', 0, 's905', 5000)
+car_1 = Car('mersedes', 1, 's40', 250)
+car_2 = Car('mazda', 5, 'xerox', 140)
 
 
 print(skoda_car.color)
 print(opel_car.weight)
-
 skoda_car.move()
 print(skoda_car.birthday())
 print(opel_car.birthday())
+
+track_daf.move()
+track_daf.load()
+
+car_1.move()
+car_2.move()
