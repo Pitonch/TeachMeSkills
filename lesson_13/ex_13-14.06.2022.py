b5fd8cfec9013a4a0899a74b2cc1984529e5118f
v.romanyuk <TdQ2spCU>
2022-06-15 17:42:08 +0300

    added user's post's with different types of requests

--- lesson_13/ex_13-14.06.2022.py
+++ lesson_13/ex_13-14.06.2022.py
@@ -12,7 +12,7 @@ class BaseModel(Model):
 class User(BaseModel):
     id = PrimaryKeyField()
     name = CharField(max_length=20, unique=True)
-    age = BigAutoField
+    age = IntegerField(constraints=[Check('age>0 and age<120')])
     gender = CharField(max_length=20)
     nationality = CharField(max_length=20)
 
@@ -23,27 +23,55 @@ class Posts(BaseModel):
     title = CharField(max_length=200)
     description = CharField(max_length=200)
     created = DateTimeField(null=False, default=datetime.now())
+    likes = ManyToManyField(User, on_delete='set null')
 
 
 class Comments(BaseModel):
     id = PrimaryKeyField()
-    text = CharField(max_length=200)
+    text = TextField()
     user_id = ForeignKeyField(User, on_delete='set null', null=True)
     post_id = ForeignKeyField(Posts, on_delete='set null', null=True)
 
 
-class Likes(BaseModel):
-    id = PrimaryKeyField()
-    user_id = ForeignKeyField(User, on_delete='set null', null=True)
-    post_id = ForeignKeyField(Posts, on_delete='set null', null=True)
+# class Likes(BaseModel):
+#     id = PrimaryKeyField()
+#     user_id = ForeignKeyField(User, on_delete='set null', null=True)
+#     post_id = ForeignKeyField(Posts, on_delete='set null', null=True)
+
+# Likes.drop_table()
+Comments.drop_table()
+Posts.drop_table()
+User.drop_table()
 
 
 User.create_table()
 Posts.create_table()
 Comments.create_table()
-Likes.create_table()
+# Likes.create_table()
 
-user1 = User(
-    name='Don', age=18, gender='M', nationality='USA'
+# user1 = User(name='Don', age=18, gender='M', nationality='USA')
+# user1.save()
+#
+# User.create(name='Ann', age=15, gender='F', nationality='USA')
+# User.create(name='X', age=100, gender='F', nationality='USA')
+#
+# users = User.select().execute()
+#
+# for x in users:
+#     print(x.name)
+
+data = [{'name': '1', 'age':13, 'gender':'f', 'nationality':'rus'},
+        {'name':'vfv', 'age':11, 'gender':'m', 'nationality':'rus'},
+        {'name':'sadf', 'age':35, 'gender':'m', 'nationality':'bel'},
+        {'name':'fdsh', 'age':19, 'gender':'f', 'nationality':'bel'}]
+
+# User.insert_many(data, fields=[User.name, User.age, User.gender, User.nationality]).execute()
+
+user1 = User(**data[0]).save()
+Posts.create(
+    user_id=user1,
+    title="хаха",
+    description='dhsluadbsl;'
 )
-user1.save()
+
+
